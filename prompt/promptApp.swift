feb406e3efffc14e7d8d6af5544d02d3c21f4a47
Miguel_Taboada <unmedia@me.com>
2026-02-15 17:36:54 +0100

    Initial Commit

---
+++ prompt/promptApp.swift
@@ -0,0 +1,17 @@
+//
+//  promptApp.swift
+//  prompt
+//
+//  Created by miguel on 15/2/26.
+//
+
+import SwiftUI
+
+@main
+struct promptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ promptTests/promptTests.swift
@@ -0,0 +1,17 @@
+//
+//  promptTests.swift
+//  promptTests
+//
+//  Created by miguel on 15/2/26.
+//
+
+import Testing
+@testable import prompt
+
+struct promptTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
